58660499f17e0fdad0d0fc6d4933a2d54d3e278a
AshwinSakhare <asakhare@zdatainc.com>
2024-04-01 15:23:55 -0400

    fix inference

--- point_net/inference.py
+++ point_net/inference.py
@@ -2,10 +2,14 @@ import numpy as np
 import open3d as o3d
 import torch
 from point_net import PointNetSegHead
+from pathlib import Path
 
 # Read the data
 PCD_FILEPATH = r'C:\Users\AshwinSakhare\MyDrive\GitHub\zData\koda_wayfinding\data\point_cloud_store_.1.ply'
 WEIGHTS_FILEPATH = r'C:\Users\AshwinSakhare\MyDrive\GitHub\zData\3D\point_net\trained_models\custom\seg_model_162.pth'
+PCD_FILEPATH = r'C:\Users\AshwinSakhare\Desktop\S3DIS\Area_6\conferenceRoom_1.pth'
+# PCD_FILEPATH = '/home/asakhare/data/datasets/stanford/Stanford3dDataset_v1.2_Reduced_Aligned_Version/Area_1/conferenceRoom_1.pth'
+# WEIGHTS_FILEPATH = '/home/asakhare/github/zData/3D/point_net/trained_models/custom/seg_model_162.pth'
 NUM_POINTS = 250000
 COLOR_MAP = {0 : (255, 0, 0), # ceiling - red
              1: (0, 255, 0), # floor - lime
@@ -21,13 +25,29 @@ seg_model.load_state_dict(weights)
 seg_model = seg_model.to('cuda')
 seg_model.eval()
 
-pcd = o3d.io.read_point_cloud(PCD_FILEPATH)
-points = np.asarray(pcd.points)
+if Path(PCD_FILEPATH).suffix == '.ply':
+    pcd = o3d.io.read_point_cloud(PCD_FILEPATH)
+    points = np.asarray(pcd.points)
+    colors = np.asarray(pcd.colors)
+
+else:
+    pcd = torch.load(PCD_FILEPATH)
+    points = pcd[:, :3]  # xyz points
+    points = np.asarray(points)
+    colors = pcd[:, 3:6]  # color points
+    colors = np.asarray(colors)
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points)
+pcd.colors = o3d.utility.Vector3dVector(colors / 255)
+o3d.visualization.draw_geometries([pcd])
+
+
 points = points - points.min(axis=0)
 points /= points.max(axis=0)
 points = torch.tensor(points, dtype=torch.float32)
 
-colors = np.asarray(pcd.colors)
+
 colors = colors - colors.min(axis=0)
 colors /= colors.max(axis=0)
 colors = torch.tensor(colors, dtype=torch.float32)
